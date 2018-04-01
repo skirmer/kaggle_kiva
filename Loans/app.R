3c85df8b9c6793464622933b1791e48c2af12bf5
Stephanie Kirmer <skirmer@Stephanies-MacBook-Pro.local>
2018-04-01 13:45:36 -0500

    Working on a map with more detail - right now loads too slow

--- Loans/app.R
+++ Loans/app.R
@@ -27,31 +27,61 @@ inputdata <- read_feather("~/Documents/R_Projects/kaggle_kiva/enriched_map_data.
 # Define server logic required to draw a histogram
 server <- function(input, output) {
    
-   output$mapPlot <- renderPlot({
-     loans_map <- inputdata
+   output$mapPlot <- renderPlotly({
      
      if(input$CONTINENT != "All"){
-       loans_map <- dplyr::filter(loans_map, CONTINENT == input$CONTINENT)
+       loans_mapa <- dplyr::filter(inputdata, CONTINENT == input$CONTINENT)
+       color_mapa <- dplyr::filter(inputdata, CONTINENT == input$CONTINENT)
+       
      }
      
-     if(input$METRIC != "mean_term"){
-      VARIABLE = input$METRIC
+     if(input$REGION != "All"){
+       loans_map <- dplyr::filter(loans_mapa, REGION_WB == input$REGION)
+       color_map <- dplyr::filter(color_mapa, REGION_WB == input$REGION)
      }
      
-      # generate bins based on input$bins from ui.R
+     if(input$SUBREGION != "All"){
+       loans_map <- dplyr::filter(loans_mapa, SUBREGION == input$SUBREGION)
+       color_map <- dplyr::filter(color_mapa, SUBREGION == input$SUBREGION)
+       
+     }
+     if(input$ECONOMY != "All"){
+       color_map <- dplyr::filter(color_mapa, ECONOMY == input$ECONOMY)
+       
+     }
+     if(input$INCOME_GRP != "All"){
+       color_map <- dplyr::filter(color_mapa, INCOME_GRP == input$INCOME_GRP)
+       
+     }
      
+     
+     if(input$METRIC != "mean_term" & input$ACTIVITY == "All"){
+      VARIABLE = paste0(input$METRIC, ".plain")
+     } else if(input$METRIC != "mean_term" & input$ACTIVITY != "All"){
+       VARIABLE = paste0(input$METRIC, ".activity")
+     } else if(input$METRIC == "mean_term" & input$ACTIVITY != "All"){
+       VARIABLE = "mean_term.activity"
+     }
+       else (VARIABLE = "mean_term.plain")
+     
+     if(input$ACTIVITY != "All"){
+       color_mapa <- dplyr::filter(color_map, activity == input$ACTIVITY | is.na(activity))
+     }
+     
+
       # draw the map
-      ggplot(loans_map, aes(x=long, y=lat, group=NAME))+
+    themap <- ggplot(loans_mapa, aes(x=long, y=lat, group=NAME, text = paste('Name: ', NAME)))+
         theme(panel.background=element_rect(fill="white", color="black"),
               axis.ticks = element_blank(),
               axis.text.x = element_blank(),
               axis.text.y = element_blank(),
               axis.title = element_blank())+
-        geom_polygon(data= loans_map, aes(x=long, y=lat, group=group, fill= get(eval(VARIABLE))))+
-        geom_path(aes(x=long, y=lat, group=group), color="black", size=0.2)+
+        geom_polygon(data= color_mapa, aes(x=long, y=lat, group=group, fill= get(eval(VARIABLE))))+
+        geom_path(data = loans_mapa, aes(x=long, y=lat, group=group), color="black", size=0.2)+
         coord_quickmap()+
         labs(title="")
       
+    ggplotly(themap, tooltip = c("text"))
    })
 }
 
@@ -60,23 +90,39 @@ server <- function(input, output) {
 ui <- fluidPage(
   
   # Application title
-  titlePanel("Loan Data by Country"),
+  titlePanel("Kiva Loan Data by Country"),
   
   # Sidebar with a slider input for number of bins 
   sidebarLayout(
     sidebarPanel(
       selectInput("CONTINENT",
                            "CONTINENT:",
+                  selected = "Asia", 
                            c("All",  sort(trimws(unique(as.character(inputdata$CONTINENT))))))
+      ,       selectInput("REGION",
+                          "REGION:",
+                          c("All",  sort(trimws(unique(as.character(inputdata$REGION_WB))))))
+      ,       selectInput("SUBREGION",
+                          "SUBREGION:",
+                          c("All",  sort(trimws(unique(as.character(inputdata$SUBREGION))))))
+      ,       selectInput("ECONOMY",
+                          "ECONOMY:",
+                          c("All",  sort(trimws(unique(as.character(inputdata$ECONOMY))))))
+      ,       selectInput("INCOME_GRP",
+                          "INCOME_GRP:",
+                          c("All",  sort(trimws(unique(as.character(inputdata$INCOME_GRP))))))
+      ,       selectInput("ACTIVITY",
+                          "ACTIVITY:",
+                          c("All",  sort(trimws(unique(as.character(inputdata$activity))))))
       
      , selectInput("METRIC",
                   "METRIC:",
-                  c("mean_term" ,   "mean_lenders", "mean_amt")
+                  c("mean_term", "mean_lenders", "mean_amt", "mean_f_term", "mean_f_lenders", "mean_f_amt")
     )),
     
     # Show a plot of the generated distribution
     mainPanel(
-      plotOutput("mapPlot")
+      plotlyOutput("mapPlot")
     )
   )
 )
--- kiva_eda.R
+++ kiva_eda.R
@@ -22,20 +22,62 @@ loans <- read.csv("kiva_loans.csv", stringsAsFactors = FALSE)
 #loan_themes <- read.csv("loan_theme_ids.csv", stringsAsFactors = FALSE)
 #loan_themes_region <- read.csv("loan_themes_by_region.csv", stringsAsFactors = FALSE)
 
+loans$any_female <- grepl("female", loans$borrower_genders)
 
-# Summarize
+# Summarize by country
 sum_loans <- loans %>%
   group_by(country) %>%
   summarize(
-    mean_term = mean(term_in_months, na.rm = T)
-    , mean_lenders = mean(lender_count, na.rm = T)
-    , mean_amt = mean(loan_amount, na.rm=T)
+    mean_term.plain = mean(term_in_months, na.rm = T)
+    , mean_lenders.plain = mean(lender_count, na.rm = T)
+    , mean_amt.plain = mean(loan_amount, na.rm=T)
+    , mean_f_term.plain = mean(term_in_months[any_female==1], na.rm = T)
+    , mean_f_lenders.plain = mean(lender_count[any_female==1], na.rm = T)
+    , mean_f_amt.plain = mean(loan_amount[any_female==1], na.rm = T)
   )
 
+# by region
+sum_loans_region <- loans %>%
+  group_by(country, region) %>%
+  summarize(
+    mean_term.region = mean(term_in_months, na.rm = T)
+    , mean_lenders.region = mean(lender_count, na.rm = T)
+    , mean_amt.region = mean(loan_amount, na.rm=T)
+    , mean_f_term.region = mean(term_in_months[any_female==1], na.rm = T)
+    , mean_f_lenders.region = mean(lender_count[any_female==1], na.rm = T)
+    , mean_f_amt.region = mean(loan_amount[any_female==1], na.rm = T)
+  )
+
+# Summarize by country and sector of loan
+sum_loans_sector <- loans %>%
+  group_by(country, sector) %>%
+  summarize(
+    mean_term.sector = mean(term_in_months, na.rm = T)
+    , mean_lenders.sector = mean(lender_count, na.rm = T)
+    , mean_amt.sector = mean(loan_amount, na.rm=T)
+    , mean_f_term.sector = mean(term_in_months[any_female==1], na.rm = T)
+    , mean_f_lenders.sector = mean(lender_count[any_female==1], na.rm = T)
+    , mean_f_amt.sector = mean(loan_amount[any_female==1], na.rm = T)
+  )
+
+# Summarize by country and activity of loan
+sum_loans_activity <- loans %>%
+  group_by(country, activity) %>%
+  summarize(
+    mean_term.activity = mean(term_in_months, na.rm = T)
+    , mean_lenders.activity = mean(lender_count, na.rm = T)
+    , mean_amt.activity = mean(loan_amount, na.rm=T)
+    , mean_f_term.activity = mean(term_in_months[any_female==1], na.rm = T)
+    , mean_f_lenders.activity = mean(lender_count[any_female==1], na.rm = T)
+    , mean_f_amt.activity = mean(loan_amount[any_female==1], na.rm = T)
+  )
 
 source("gis.R")
 
-loans_map <- merge(sum_loans, shapefile1_df2, by.x = "country", by.y="NAME_CIAWF", all.y = T)
+loans_map <- merge(sum_loans, shapefile1_df2, by.x = "country", by.y="NAME_CIAWF", all = T)
+loans_map <- merge(loans_map, sum_loans_activity, by.x = "country", by.y="country", all = T, suffixes = c(".plain", ".activity"))
+loans_map <- merge(loans_map, sum_loans_sector, by.x = "country", by.y="country", all = T, suffixes = c(".plain", ".sector"))
+
 loans_map <- arrange(loans_map, order)
 
 feather::write_feather(loans_map, "enriched_map_data.feather")
