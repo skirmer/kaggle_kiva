08fe1ece48fde1c02df252eddbd6fc587c3779b5
Stephanie Kirmer <skirmer@Stephanies-MacBook-Pro.local>
2018-04-07 19:01:20 -0500

    Converted currencies, added map functionality

--- Loans/app.R
+++ Loans/app.R
@@ -22,37 +22,49 @@ library(plotly)
 library(lubridate)
 library(feather)
 
-inputdata <- read_feather("~/Documents/R_Projects/kaggle_kiva/enriched_map_data.feather")
-country_names <- read_feather("~/Documents/R_Projects/kaggle_kiva/country_names.feather")
+inputdata <- read_feather("enriched_map_data.feather")
+country_names <- read_feather("country_names.feather")
 
 
 #listing features
-features <- list("Mean Term of Loan (Months)" = "mean_term"
-     , "Mean Loan Amount (Local Currency)" = "mean_amt"
+features <- list("Total Loans" = "total_loans"
+     , "Mean Term of Loan (Months)" = "mean_term"
+     , "Mean Loan Amount (USD)" = "mean_amt"
+     , "Median Loan Amount (USD)" = "median_amt"
      , "Mean Number of Lenders per Loan" = "mean_lenders"
      , "Mean Term of Loan (Months), Female Recipient" = "mean_f_term"
-     , "Mean Loan Amount (Local Currency), Female Recipient" = "mean_f_amt"
+     , "Mean Loan Amount (USD), Female Recipient" = "mean_f_amt"
+     , "Median Loan Amount (USD), Female Recipient" = "median_f_amt"
      , "Mean Number of Lenders per Loan, Female Recipient" = "mean_f_lenders"
-     , "Male:Female Ratio of Mean Loan Amount" = "gender_ratio_mean_amt")
+     , "Male:Female Ratio of Mean Loan Amount" = "gender_ratio_mean_amt"
+     , "Male:Female Ratio of Median Loan Amount" = "gender_ratio_median_amt")
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
    
-   output$mapPlot <- renderPlot({
+   output$mapPlot <- renderPlot(width = 1000, height = 1000, {
      loans_map <- inputdata
-     color_map <- inputdata
+     
      country_names <- country_names
      features <- features
      
-     if(input$CONTINENT != "All"){
+     if(input$OMIT_SMALL == "True"){
+       color_map <- inputdata[inputdata$total_loans > 5,]
+     } else {color_map <- inputdata}
+     
+     if(input$CONTINENT != "All" & input$COUNTRY == "All"){
        loans_map <- dplyr::filter(loans_map, CONTINENT == input$CONTINENT)
        color_map <- dplyr::filter(color_map, CONTINENT == input$CONTINENT) 
        country_names <- dplyr::filter(country_names, CONTINENT == input$CONTINENT) 
-     } else { 
+     } else if(input$CONTINENT == "All" & input$COUNTRY == "All") { 
        loans_map <- loans_map
        color_map <- color_map
        country_names <- dplyr::filter(country_names, country %in% sample(country_names$country, 0))
-       }
+     } else if(input$COUNTRY != "All"){
+       loans_map <- dplyr::filter(loans_map, country == input$COUNTRY)
+       color_map <- dplyr::filter(color_map, country == input$COUNTRY) 
+       country_names <- dplyr::filter(country_names, country == input$COUNTRY) 
+     } 
      
      if(input$ECONOMY != "All"){
        color_map <- dplyr::filter(color_map, ECONOMY == input$ECONOMY|is.na(input$ECONOMY))
@@ -63,26 +75,32 @@ server <- function(input, output) {
        color_map <- dplyr::filter(color_map, INCOME_GRP == input$INCOME_GRP|is.na(input$INCOME_GRP))
      }
      
-     if(input$METRIC != "Mean Term of Loan (Months)"){
+     if(input$METRIC != "Total Loans" ){
       VARIABLE = features[[input$METRIC]]
-     } else {VARIABLE = "mean_term"}
+     } else {VARIABLE = "total_loans"}
      
       # generate bins based on input$bins from ui.R
      
       # draw the map
-      ggplot(loans_map, aes(x=long, y=lat, group=country))+
+      plot1 <- ggplot(loans_map, aes(x=long, y=lat, group=country))+
         theme(panel.background=element_rect(fill="white", color="black"),
               axis.ticks = element_blank(),
               axis.text.x = element_blank(),
               axis.text.y = element_blank(),
-              axis.title = element_blank())+
+              axis.title = element_blank(),
+              legend.position="bottom",
+              legend.key.width= unit(5, "cm")
+              , legend.text = element_text(size = 14)
+              , legend.title = element_text(size = 14)
+              , plot.title = element_text(size = 18))+
         scale_fill_gradient(name=input$METRIC)+
         geom_polygon(data=color_map, aes(x=long, y=lat, group=group, fill= get(eval(VARIABLE))))+
-        geom_text_repel(data=country_names, aes(long, lat, label = country), size=2)+
         geom_path(data=loans_map, aes(x=long, y=lat, group=group), color="black", size=0.2)+
+        geom_label_repel(data=country_names, aes(long, lat, label = country), size=4, color = "black")+
         coord_quickmap()+
         labs(title=paste0(input$CONTINENT, ": Country Level Analysis of ", input$METRIC))
       
+      plot1 
    })
    
 
@@ -95,7 +113,7 @@ ui <- fluidPage(
   # Application title
   titlePanel("Kiva Loan Data by Country"),
 
-  "Welcome! To start exploring the data, select a continent and begin to drill down."  ,br(),
+  "Welcome! This app is still a work in progress, but please feel free to use it. To start exploring the data, select a continent and begin to drill down."  ,br(),
   "Source of shapefiles and general national economic data:", a("Natural Earth Data", 
       href= "http://www.naturalearthdata.com/downloads/110m-cultural-vectors/110m-admin-0-countries/"), br(),
   "Source of Kiva data:" , a("Kaggle.com", 
@@ -105,35 +123,48 @@ ui <- fluidPage(
   # 
   
   # Sidebar with a slider input for number of bins 
-  sidebarLayout(
-    sidebarPanel(
-      selectInput("CONTINENT",
+  fluidRow(
+    column(3,selectInput("CONTINENT",
                            "Continent:",
                   selected = "Asia"
-                  ,         c("All",  sort(trimws(unique(as.character(inputdata$CONTINENT))))))
+                  ,         c("All",  sort(trimws(unique(as.character(inputdata$CONTINENT)))))))
     
-      , selectInput("ECONOMY",
+     ,  column(3,selectInput("COUNTRY",
+                             "Country:",
+                             selected = "All"
+                             ,         c("All",  sort(trimws(unique(as.character(country_names$country)))))))
+    
+      , column(3,selectInput("ECONOMY",
                           "Economic Classification:",
-                          c("All",  sort(trimws(unique(as.character(inputdata$ECONOMY))))))
+                          c("All",  sort(trimws(unique(as.character(inputdata$ECONOMY)))))))
       
-      , selectInput("INCOME_GRP",
+      , column(3,selectInput("INCOME_GRP",
                           "National Income Classification:",
-                          c("All",  sort(trimws(unique(as.character(inputdata$INCOME_GRP))))))
+                          c("All",  sort(trimws(unique(as.character(inputdata$INCOME_GRP)))))))
       
-     , selectInput("METRIC",
+    , column(3,selectInput("OMIT_SMALL",
+                           "Omit Countries Below 5 Loans:",
+                           c("True",  "False")))
+    
+     , column(3,selectInput("METRIC",
                   "Metric:"
-                  , selected = "Mean Term of Loan (Months)"
-                  , c("Mean Term of Loan (Months)"
-                      , "Mean Loan Amount (Local Currency)"
+                  , selected = "Total Loans" 
+                  , c(
+                    "Total Loans" 
+                    , "Mean Term of Loan (Months)"
+                      , "Mean Loan Amount (USD)"
+                      , "Median Loan Amount (USD)"
                       , "Mean Number of Lenders per Loan"
                       , "Mean Term of Loan (Months), Female Recipient"
-                      , "Mean Loan Amount (Local Currency), Female Recipient"
+                      , "Mean Loan Amount (USD), Female Recipient" 
+                      , "Median Loan Amount (USD), Female Recipient"
                       , "Mean Number of Lenders per Loan, Female Recipient"
-                      , "Male:Female Ratio of Mean Loan Amount")
+                      , "Male:Female Ratio of Mean Loan Amount"
+                      , "Male:Female Ratio of Median Loan Amount")
     )),
     
     # Show a plot of the generated distribution
-    mainPanel(
+    column(12,
       plotOutput("mapPlot")
     )
   )
--- kiva_eda.R
+++ kiva_eda.R
@@ -12,30 +12,25 @@ library(RColorBrewer)
 library(maptools)
 library(classInt)
 library(broom)
+library(quantmod)
 
-# General map with nations overlay- anything summarized by nation is fine
-# lay on top of that the region points?
-
-
-loans <- read.csv("kiva_loans.csv", stringsAsFactors = FALSE)
-#regions <- read.csv("kiva_mpi_region_locations.csv", stringsAsFactors = FALSE)
-#loan_themes <- read.csv("loan_theme_ids.csv", stringsAsFactors = FALSE)
-#loan_themes_region <- read.csv("loan_themes_by_region.csv", stringsAsFactors = FALSE)
-
-loans$any_female <- grepl("female", loans$borrower_genders)
-
+source("kiva_loan_cleaning.R")
 
 # Summarize
 sum_loans <- loans %>%
   group_by(country) %>%
   summarize(
-    mean_term = mean(term_in_months, na.rm = T)
+    total_loans = n()
+    , mean_term = mean(term_in_months, na.rm = T)
     , mean_lenders = mean(lender_count, na.rm = T)
-    , mean_amt = mean(loan_amount, na.rm=T)
+    , mean_amt = mean(usd_amt, na.rm=T)
+    , median_amt = median(usd_amt, na.rm=T)
     , mean_f_term = mean(term_in_months[any_female==1], na.rm = T)
     , mean_f_lenders = mean(lender_count[any_female==1], na.rm = T)
-    , mean_f_amt = mean(loan_amount[any_female==1], na.rm = T)
+    , mean_f_amt = mean(usd_amt[any_female==1], na.rm = T)
+    , median_f_amt = median(usd_amt[any_female==1], na.rm = T)
     , gender_ratio_mean_amt = mean_f_amt/mean_amt
+    , gender_ratio_median_amt = median_f_amt/median_amt
   )
 
 
@@ -44,19 +39,17 @@ source("gis.R")
 loans_map <- merge(sum_loans, shapefile1_df2, by.x = "country", by.y="NAME_CIAWF", all.y = T)
 loans_map <- arrange(loans_map, order)
 
-feather::write_feather(loans_map, "enriched_map_data.feather")
+feather::write_feather(loans_map, "Loans/enriched_map_data.feather")
 
 ###
 #loans_map <- feather::read_feather("enriched_map_data.feather")
-
-# Looking at adding labels without going to plotly
 
 country_names <- aggregate(cbind(long, lat) ~ country, data=loans_map, 
                     FUN=function(x)mean(range(x)))
 loans_map2 <- merge(loans_map[,c("country", "CONTINENT", "ECONOMY", "INCOME_GRP")], country_names, by="country", all = T, suffixes = c(".plain", ".centroid"))
 loans_map2 <- unique(loans_map2)
 
-feather::write_feather(loans_map2, "country_names.feather")
+feather::write_feather(loans_map2, "Loans/country_names.feather")
 
 rm(shapefile1)
 rm(shapefile1_df)
